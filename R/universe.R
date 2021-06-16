328b22579d40fe3adb95acb3606eea11428056d9
Kevin Ummel <ummel@sas.upenn.edu>
2021-06-16 09:22:00 -0600

    universe() documentation edit

--- R/universe.R
+++ R/universe.R
@@ -1,7 +1,7 @@
 #' Open the fusionACS Universal Survey Dictionary
 #'
 #' @description
-#' Opens the 'universe' Shiny app (/dictionary); i.e. fusionACS Universal Survey Dictionary.
+#' Opens the 'universe' Shiny app (/universe); i.e. fusionACS Universal Survey Dictionary.
 #'
 #' @return Opens the app in a new browser window. R session is occupied while that window is open.
 #'
