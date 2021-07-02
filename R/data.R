0e4ba4eb703d1d72f24aa7af961a0dc2633a75ff
Kevin Ummel <ummel@sas.upenn.edu>
2021-07-02 16:28:29 -0600

    Resolves #15.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Block group centroids circa 2010
+#'
+#' Population-weighted block group centroids. `sf` points object. Useful for assigning any coordinate-based spatial features to block groups to create geographic concordance; e.g. using \code{\link[sf]{st_nearest_feature}}.
+#'
+#' @format A `sf` spatial data frame.
+#' \describe{
+#'   \item{state}{State code}
+#'   \item{county10}{County code}
+#'   \item{tract10}{Tract code}
+#'   \item{bg10}{Blcok group code}
+#'   \item{geometry}{Centroid coordinates as `sfc_POINT` class}
+#'   }
+#' @source \url{https://www2.census.gov/geo/docs/reference/cenpop2010/blkgrp/}
+"bg_centroids"
