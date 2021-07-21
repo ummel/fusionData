ce7a8186e0b6a600db516679f2e2f169b206131e
Kevin Ummel <ummel@sas.upenn.edu>
2021-07-21 08:35:19 -0600

    Accomodate NA values in geographic variables. Edits to allow CEX fusion.

--- R/assignLocation.R
+++ R/assignLocation.R
@@ -68,48 +68,80 @@ assignLocation <- function(harmonized, m = 1) {
   # Ensure the factor levels are consistent between 'glink' and 'D' (possible that latter could be missing some levels)
   for (v in gdonor) set(D, j = v, value = factor(D[[v]], levels = levels(glink[[v]]), ordered = is.ordered(glink[[v]])))
 
+  # Which geographic intersection variables should be returned as "loc.." variables in output results?
+  # This is restricted to 'gdonor' variables with no missing values in the donor microdata
+  gkeep <- names(which(!sapply(D[, ..gdonor], anyNA)))
+  gkeep <- setdiff(gkeep, gtarget)  # This simply excludes "state" in the event it is present in donor data (to avoid conflict with "state" used for PUMA identification in 'gtarget')
+
   #---
 
   # Assign integer ID for each geographic intersection defined by the 'gdonor' variables
   D[, id := .GRP, by = gdonor]
-  #setorder(D, id)  # NECESSARY?
-
-  # Add 'id' assignment to 'glink' observations
-  glink[D, id := i.id, on = gdonor]
-  #setorder(glink, id) # NECESSARY?
-
-  # If any 'puma10' are NA, that indicates a geo group in donor was not present in geolink
-  stopifnot(!anyNA(glink$puma10))
 
   # Report number of unique geographic intersections
   cat("Identified", max(D$id), "geographic intersections in the donor...\n")
 
+  #---
+
+  # In event that there are NA values in 'D', building the processed 'glink' data.table sequentially
+  # If there are no NA's, a faster data.table operation can be used
+
+  if (anyNA(D)) {
+
+    f <- function(i) {
+
+      # Subset 'D' for intersection 'i' and remove columns with NA values
+      d <- subset(D, id == i)
+      d <- d[, .SD, .SDcols = names(which(!sapply(d, anyNA)))]
+
+      if (ncol(d) < ncol(D)) {
+        gsum <- unique(c(gdonor, gtarget))
+        gtemp <- glink[, .(W = sum(W)), by = gsum]
+      } else {
+        gtemp <- copy(glink)
+      }
+
+      # Add 'id' assignment to 'gtemp' observations
+      gtemp[d, id := i.id, on = intersect(gdonor, names(d))]
+
+      # For each PUMA, calculate proportion of households in each intersection (intersection 'i' or NA, in this case)
+      # This is used to assign the "naive"/default probability that a household in a given PUMA is also located in intersection 'id'
+      gtemp[, puma_share := W / sum(W), by = gtarget]
+
+      # Restrict output to geographic areas within intersection 'i'
+      return(subset(gtemp, !is.na(id)))
+
+    }
+
+    # Process all intersections and bind results
+    glink <- lapply(1:max(D$id), f) %>%
+      rbindlist(fill = TRUE)
+
+  } else {
+
+    # Add 'id' assignment to 'glink' observations
+    glink[D, id := i.id, on = gdonor]
+
+    # For each PUMA, calculate proportion of households in each intersection
+    # This is used to assign the "naive"/default probability that a household in a given PUMA is also located in intersection 'id'
+    glink[, puma_share := W / sum(W), by = gtarget]
+
+  }
+
   #-----
 
-  # CHECK FOR SAFETY - manual check that the 'id' assignment was correct
-  c1 <- D %>%
-    select(all_of(c(gdonor, "id"))) %>%
-    distinct() %>%
-    arrange(id)
-
-  c2 <- glink %>%
-    filter(!is.na(id)) %>%
-    select(all_of(c(gdonor, "id"))) %>%
-    distinct() %>%
-    arrange(id)
-
-  stopifnot(identical(c1, c2))
+  # Check for any 'id' values in 'D' that could be find a match in 'glink'
+  miss <- sort(setdiff(unique(D$id), unique(glink$id)))
+  if (length(miss) > 0) stop("There are geographic intersections in the donor microdata that could not be matched to the 'geo_concordance' file.\nYou should check that the geographic variables in the two datasets are defined identically.")
 
   #-----
 
-  # For each PUMA, calculate proportion of households in each intersection
-  # That is, conditional on a HH being in intersection 'id', what is the likelihood that it is located in each PUMA?
-  glink[, puma_share := W / sum(W), by = gtarget]
+  # Restrict 'glink' to only necessary columns?
+  #glink <- subset(glink, select = c(gtarget, "id", "puma_share"))
 
   #-----
 
   # Donor output from harmonize() with 'id' merged
-  #set(data, j = gdonor, value = NULL)
   D <- harmonized[[1]] %>%
     as.data.table() %>%
     merge(D, by = did)
@@ -119,29 +151,30 @@ assignLocation <- function(harmonized, m = 1) {
     as.data.table()
 
   # Variables to use for distance/similarity calculation
-  X <- setdiff(intersect(names(D), names(R)), "weight")
+  X <- setdiff(intersect(names(D), names(R)), c("weight", gdonor, gtarget))
 
   #-----
 
   #i <- 5
   #N <- 1000
 
-  sampleIntersection <- function(i, N = 1000) {
+  sampleIntersection <- function(i, N = 500) {
 
+    # Subset 'D' for intersection 'i' and remove columns with NA values
     d <- subset(D, id == i)
 
     # puma_share is the naive probability that the respondent is in intersection 'id'
     # When default sample weight is multiplied by 'puma_share', we get an estimate of the intersection-specific sample weight
     r <- R %>%
-      merge(subset(glink, id == i, c(gtarget, "id", "puma_share")), by = gtarget) %>%
-      mutate(weight = weight * puma_share) %>%
+      merge(subset(glink, id == i), by = gtarget, allow.cartesian = TRUE) %>%
+      mutate(weight = weight * puma_share) %>% # "naive" likelihood of selecting each household in 'r'
       slice_sample(n = min(N, nrow(.)))
 
-    # Gower distance for top-n most similar respondents
+    # Gower distance for top-N most similar respondents
     G <- gower::gower_topn(x = d[, ..X], y = r[, ..X], n = nrow(r), nthread = mc.cores)
 
     # Initial 'weight' of each recipient observation
-    # This is the "naive" likelihood of selection
+    # This is the "naive" likelihood of selecting each household in 'r'
     P0 <- G$index
     P0[] <- r$weight[G$index]
 
@@ -149,16 +182,17 @@ assignLocation <- function(harmonized, m = 1) {
     zero <- G$distance == 0
     if (any(zero)) G$distance[zero] <- 0.5 * min(G$distance[!zero])
 
-    # Adjusted likelihood of selection
+    # Adjusted likelihood of selection (naive probability divided by Gower's distance)
     P <- P0 / G$distance
 
-    # Random sampling of 'm' implicates for each respondent in 'd', based on combined probability of selection (P0 * P1)
+    # Random sampling of 'm' implicates for each respondent in 'd', based on adjusted probability of selection (P)
     S <- sapply(X = 1:ncol(P),
                 FUN = function(i) sample(x = G$index[, i], size = m, replace = TRUE, prob = P[, i]),
                 simplify = TRUE)
 
-    # Assemble output; imputed (state, PUMA, recipient ID) and actual (gdonor) geographic variables for each donor household in 'd'
-    dkeep <- c(did, gdonor)
+    # Assemble output
+    # Includes imputed variables (state, PUMA, recipient ID) and actual (gdonor) geographic variables for each donor household in 'd'
+    dkeep <- c(did, gkeep)
     rkeep <- c(rid, gtarget)
     out <- cbind(d[rep(1:nrow(d), times = m), ..dkeep],
                  r[as.vector(t(S)), ..rkeep])
@@ -179,7 +213,7 @@ assignLocation <- function(harmonized, m = 1) {
                          cl = 1L,  # This defaults to lapply() execution but gives a nice progress bar/timer
                          N = 1000) %>%
     rbindlist() %>%
-    setcolorder(c(did, rid, gtarget, gdonor))
+    setcolorder(c(did, rid, gtarget, gkeep))
 
   # Calculate 'weight_adjustment' column
   # When 'D' is merged with microdata, the household "weight" is multiplied by "weight_adjustment" to arrive at correct total sample weight
@@ -196,7 +230,8 @@ assignLocation <- function(harmonized, m = 1) {
 
   cat("Assigning location variables to recipient observations...\n")
 
-  # Assign 'gdonor' variables to each recipient household
+  # NOTE: Originally, this code block referred to the 'gdonor' variables, but this was switched to 'gkeep' for simplicity and to avoid dealing with NA's for some geographic variables in donor microdata
+  # Assign 'gkeep' variables to each recipient household
   # There is no guarantee that PUMA's are uniquely identified by the 'gdonor' variables
   # Instead, PUMA boundaries may span intersection boundaries
   # Consequently, the assignment of 'gdonor' variables to the recipient needs to be a random sample (i.e. households in the same PUMA can be assigned to different 'gdonor' intersections)
@@ -210,7 +245,7 @@ assignLocation <- function(harmonized, m = 1) {
   R[ , N := .N, by = gtarget]
   setorderv(R, cols = gtarget)
 
-  # Add the 'N' count variable to 'glink'; this gives the number of households in each unique PUMA
+  # Add the 'N' count variable to 'glink'; this gives the number of sampled households in each unique PUMA
   # For each PUMA in 'glink', randomly sample 'N' rows, where probability of selection is equal to 'puma_share'
   # The result is a data.table with same number of rows as 'R'
   # Order the rows by PUMA so it is aligned with 'R' and can be cbind'd below
@@ -218,18 +253,18 @@ assignLocation <- function(harmonized, m = 1) {
   glink <- glink[glink[, .I[sample(.N, size = N, prob = puma_share, replace = TRUE)], by = gtarget]$V1]
   setorderv(glink, cols = gtarget)
 
-  # Assign 'gdonor' variables for each recipient household
+  # Assign 'gkeep' variables for each recipient household
   stopifnot(nrow(R) == nrow(glink))
-  R <- cbind(R[, ..rid], glink[, ..gdonor])
+  R <- cbind(R[, ..rid], glink[, ..gkeep])
   #setkeyv(R, cols = rid)  # Set key so it can be merged quickly on 'rid' within prepare()?
 
   #---
 
-  # Rename the 'gdonor' variables to include the "loc.." prefix
+  # Rename the 'gkeep' variables to include the "loc.." prefix
   # This identifies them as spatial variables, but "loc" is reserved for variables that are actually known and not imputed
-  lvars <- paste0("loc..", gdonor)
-  setnames(D, old = gdonor, new = lvars)
-  setnames(R, old = gdonor, new = lvars)
+  lvars <- paste0("loc..", gkeep)
+  setnames(D, old = gkeep, new = lvars)
+  setnames(R, old = gkeep, new = lvars)
 
   # Assemble into final results list
   # Assign attribute indicating the geographic intersection (i.e. "location") variables
