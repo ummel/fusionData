3d7c9054cbcf310c9a531ba2a22c44a215c10462
Kevin Ummel <ummel@sas.upenn.edu>
2021-08-17 13:35:13 -0700

    Allow mortgage interest components in CEI fusion.

--- production/Fuse CEX_2015-2019 to ACS_2019.R
+++ production/Fuse CEX_2015-2019 to ACS_2019.R
@@ -14,7 +14,16 @@ prep <- prepare(donor = "CEI_2015-2019",
 data <- assemble(prep,
                  window = 2)
 
-rm(prep)
+# Add custom variable "mortint_share"
+temp <- read_fst("survey-processed/CEX/CEI/CEI_2015-2019_H_processed.fst") %>%
+  mutate(mortgage = mrtgip + mrtgpp,
+         mortint_share = ifelse(mortgage == 0, 0, mrtgip / mortgage)) %>%
+  select(cei_hid, mortint_share)
+
+data$`CEI_2015-2019` <- data$`CEI_2015-2019` %>%
+  left_join(temp, by = "cei_hid")
+
+rm(prep, temp)
 gc()
 
 #-----
@@ -58,7 +67,9 @@ data$ACS_2019 <- data$ACS_2019 %>%
 
 # Select fusion variables from among the full potential set
 # Excludes variables shared with RECS
+# Include the custom "mortint_share" variable
 fvars <- setdiff(attr(data, "fusion.vars"), c("cntralac", "windowac", "elec", "ngas", "ofuel"))
+fvars <- c(fvars, "mortint_share")
 
 # Variables in CEI microdata that we'd rather just ignore entirely
 # These are variables that probably don't need to be in the microdata at all, or could be harmonized with ACS with some additional effort, etc.
@@ -78,12 +89,21 @@ fit <- train(data = data$`CEI_2015-2019`,
 #-----
 
 # Fuse variables (18 min)
-# All of the numeric variables can be converted to integer (either expenditure or financial balances variables)
-# Add ACS 2019 household ID column
+sim <- fuse(data = data$ACS_2019, train.object = fit)
 
-sim <- fuse(data = data$ACS_2019, train.object = fit) %>%
+# Add primary mortgage interest and principal expenditure variables manually
+# Remaining numeric variables can be converted to integer (either expenditure or financial balances variables)
+# Also adds ACS 2019 household ID column
+temp <- read_fst("survey-processed/ACS/2019/ACS_2019_H_processed.fst", columns = c("acs_2019_hid", "mortgage"))
+stopifnot(all(temp$acs_2019_hid == data$ACS_2019$acs_2019_hid))
+sim <- sim %>%
+  cbind(temp) %>%
+  mutate(
+    mrtgip = round(mortint_share * mortgage),
+    mrtgpp = mortgage - mrtgip,
+  ) %>%
+  select(-mortgage, -mortint_share) %>%
   mutate_if(is.numeric, ~ as.integer(round(.x))) %>%
-  cbind(data$ACS_2019["acs_2019_hid"]) %>%
   select(acs_2019_hid, everything())
 
 stopifnot(!anyNA(sim))
