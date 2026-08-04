302b67b0974cc3005546c57b3e05d9f248663981
ummel <ummel@berkeley.edu>
2026-08-04 10:52:41 -0600

    Set HPC working directory on package load.

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,32 @@
 .onLoad <- function(libname, pkgname) {
+
   # Prevent styler cache message; See ?styler::caching
   options(styler.cache_root = "styler")
 
   # Create default option value for number of cores
   options(fusionData.cores = max(1L, parallel::detectCores() - 1L))
 
-  # Check that the current working directory is /fusionData
-  if (basename(getwd()) != "fusionData") {
-    stop("The fusionData package requires the working directory to be /fusionData")
-  }
 }
 
 .onAttach <- function(libname, pkgname) {
+
+  # 1. Yale HPC Automated Working Directory Configuration
+  yale_target_dir <- "/gpfs/milgram/project/rao/shared/fusionACS/fusionData"
+
+  # Check if running within Yale HPC environment and target path exists
+  is_yale_hpc <- dir.exists("/gpfs/milgram") & Sys.getenv("SLURM_CLUSTER_NAME") == "milgram"
+
+  if (is_yale_hpc && dir.exists(yale_target_dir)) {
+    setwd(yale_target_dir)
+    packageStartupMessage("Yale HPC Milgram detected. Working directory set to:\n", yale_target_dir)
+  }
+
+  # Warn if directory is still not /fusionData after attempting HPC adjustment
+  if (basename(getwd()) != "fusionData") {
+    packageStartupMessage("Warning: The fusionData package requires the working directory to be /fusionData")
+  }
+
+  # 2. Package Version and Remote Update Check
   current_ver <- utils::packageVersion("fusionData")
 
   # Always print package info first
